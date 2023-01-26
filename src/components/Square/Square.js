7c2b66a8b599317643d6b5b66e1d92c683d1104f
m-ghiringhelli <mghiring@gmail.com>
2023-01-26 15:19:04 -0800

    setup game state to play and determine winner

--- src/components/Square/Square.js
+++ src/components/Square/Square.js
@@ -1,24 +1,65 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { GameContext } from '../../context/GameContext';
 import Board from '../Board/Board';
 import './Square.css';
 
 export default function Square({ square }) {
-  const { active, currentPlayer, board, setBoard } = useContext(GameContext);
+  const { active, setActive, currentPlayer, setCurrentPlayer, board, setBoard, setGameMessage } = useContext(GameContext);
+
   const handleClick = () => {
     const clickedSquare = square.place;
     if (square.content) return;
     if (!active) return;
     setBoard((prevState) => (
       prevState.map((sqr) => (
-        sqr.place === clickedSquare ? { ...sqr, content: 'clicked!' } : sqr
+        sqr.place === clickedSquare ? { ...sqr, content: `${currentPlayer}` } : sqr
       ))
     ));
-    console.log(`clicked ${square.place}, ${currentPlayer}`);
-    // console.log('new board', board);
+
+    setCurrentPlayer((prevState) => (
+      prevState === 'X' ? 'O' : 'X'
+    ));
   };
 
+  function matchThree(a, b, c) {
+    if (!a || !b || !c) return false;
+    return ((a === b) && (b === c));
+  }
+
+  function checkWin() {
+    const win = 
+      active ?
+        matchThree(board[0].content, board[1].content, board[2].content) ||
+        matchThree(board[3].content, board[4].content, board[5].content) ||
+        matchThree(board[6].content, board[7].content, board[8].content) ||
+        matchThree(board[0].content, board[3].content, board[6].content) ||
+        matchThree(board[1].content, board[4].content, board[7].content) ||
+        matchThree(board[2].content, board[5].content, board[8].content) ||
+        matchThree(board[0].content, board[4].content, board[8].content) ||
+        matchThree(board[2].content, board[4].content, board[6].content) :
+        false
+    ;
+    return win;
+  }
+
+  function checkCatsGame() {
+    let cats = true;
+    for (let sqr of board) {
+      if (!sqr.content) cats = false;
+    }
+    return cats;
+  }
+
+  useEffect(() => {
+    const win = checkWin();
+    const cats = checkCatsGame();
+    const player = currentPlayer === 'X' ? 'O' : 'X';
+    win && setGameMessage(`${player} wins!`);
+    cats && setGameMessage('Cats game!');
+    (win || cats) && setActive(!active);
+  }, [board]);
+
   return (
-    <div className='square' onClick={handleClick}>{`${square.place}`}</div>
+    <div className='square' onClick={handleClick}>{`${square.content}`}</div>
   );
 }
